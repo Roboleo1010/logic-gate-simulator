b84c62739e54f8abc96c1a174023a8722d451b4b
Leon Voß <leon.j.voss@gmail.com>
2020-12-15 22:32:00 +0100

    made toolbar smaller

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import App from './components/app/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import './styles/design.scss';
+import './design.scss';
 
 ReactDOM.render(
   <App />,
